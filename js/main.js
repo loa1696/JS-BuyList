397c039fc5628efd73da693d7355a1a066030f41
loa1696 <loa1696@gmail.com>
2016-01-20 12:22:47 +0200

    AddJavaScript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Student on 20.01.2016.
+ */
+$(function(){
+//This	function	will	be	called
+//	after	the	DOM	was	loaded	and	ready
+//Here	we	can	place	all	jQuery	Code
+    $('button').click(function(){
+        $(this).css('font-size','25px');
+    });
+});
