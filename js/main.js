f7f5c10df8a276186ccf8b88e5be5198e16730d7
loa1696 <loa1696@gmail.com>
2016-02-03 00:09:37 +0200

    END

--- js/main.js
+++ js/main.js
@@ -4,78 +4,83 @@
 /**
  * Created by Student on 20.01.2016.
  */
+
+var id = 0;
 $(function() {
 
-    $('.text-input').hide();
-    $('.button-not-buy').hide();
-    var ITEM_TEMPLATE=$(".style1");
-    var ITEM_STATS_TEMPLATE=$(".style2");
+    $("body").append(wrapEllement("Морозиво"));
+    $("body").append(wrapEllement("Яблука"));
+    $("body").append(wrapEllement("Печиво"));
 
 
-$('.addButton').click(function(){
-    var title=$('.area').val();
-    $('.area').val("");
-    if (title == "")
-    return;
-    create(title);
-});
-    function create(title){
-        var node = $(ITEM_TEMPLATE);
-        var node2 = $(ITEM_STATS_TEMPLATE);
-        node.find()
-    }
+    $(".button-not-buy").hide();
 
     $('.button-plus').click(function () {
         var num = $(this).parent().find(".counter").text();
         num = parseInt(num);
         $(this).parent().find(".counter").text(++num);
-        $("counter").text(num);
+        $(".counter").text(num);
     });
     $('.button-minus').click(function () {
         var num = $(this).parent().find(".counter").text();
         num = parseInt(num);
-        $(this).parent().find(".counter").text(++num);
-        $("counter").text(num);
-    });
-
-    $('.text').click(function () {
-        $('#text1').hide();
-        $('#text2').show()
-    });
-
-    $('#noBuy1').click(function () {
-        $('#buy1').show();
-        $('#buttonPlus1').show();
-        $('#buttonMinus1').show();
-        $('#del1').show();
-        $('#noBuy1').hide();
-        $('#coun1').show();
-        $('#text1').css('text-decoration', ' none');
-    });
-    $('#del1').click(function() {
-        $('#div1').hide();
-    });
-    $('#del2').click(function() {
-        $('#div2').hide();
-    });
-    $('#del3').click(function() {
-        $('#div3').hide();
+        $(this).parent().find(".counter").text(--num);
+        $(".counter").text(num);
     });
 
 
-    /*function addItem(title){-->
-     var node = $(ITEM_TEMPLATE);
-     var parent = $('<div id="segment state-ready"  style="display:none;"></div>');
-     var cont = true;
-     $('#add').click(function(){
-     $('.textarea').val();
-     var n_v =$('.textarea').val();
-     var node =$(n_v);
-     var ITEM_TEMPLATE = $('.div1').html();
-     function addItem(title){
-     var node=$(ITEM_TEMPLATE);
-     node.find(".textarea").text();
-     } */
+    $(".addButton").click(function () {
+        //Р·Р°Р±СЂР°С‚Рё РЅР°Р·РІСѓ С– РґРѕРґР°С‚Рё РІ СЃРїРёСЃРѕРє
+        var name_prod = $("#area").val();
+        $('#area').val("");
+        var wrapped_product = wrapEllement(name_prod);
+        $("body").append(wrapped_product);
+        addEvListener($(wrapped_product).find(".text"));
 
+        var product = $(wrapped_product).find(".text");
+        $(product).text(name_prod);
+
+    });
+
+
+    function wrapEllement(name_prod) {
+        var prod_div = '<div   class="style1">' +
+            '<input  class="text-input" type="text"  value="' + name_prod + '">' +
+
+            '<button  class="button-plus" type="button">+</button>' +
+            '<span  class="counter" >1</span>' +
+            '<button   class="button-minus button-minus-block" type="button">-</button>' +
+
+            '<button  class="button-buy" type="button">Куплено</button>' +
+            '<button  style="display:none" class="button-not-buy" type="button">Не куплено</button>' +
+
+            '<button class="button-delete"  type="button">x</button>' +
+            '</div>'
+        return prod_div;
+    }
+
+    function addEvListener(object) {
+        $(object).click(function () {
+            $(this).hide();
+        });
+    }
+    $('.button-buy').click(function (){
+                $('.button-buy').hide();
+                $('.button-plus').hide();
+                $('.button-minus').hide();
+                $('.button-delete').hide();
+                $('.button-not-buy').show();
+                $('.counter').hide();
+        $(product).text(name_prod.css('text-decoration',' line-through'));
+            });
+    $('.button-not-buy').click(function (){
+        $('.button-buy').show();
+        $('.button-plus').show();
+        $('.button-minus').show();
+        $('.button-delete').show();
+        $('.button-not-buy').hide();
+        $('.counter').show();
+        $(product).text(name_prod.css('text-decoration',' line-through'));
+    });
 });
 
